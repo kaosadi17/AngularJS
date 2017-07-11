ce918e3918c9b5a7e156f7c00c473d601868e6d7
amalio <amali.osadi@trycake.com>
2017-07-11 11:25:08 +0530

    added ng-model functionality

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ var module = angular.module("myMod",[]);
 module.controller("manCtrl",main);
 
 function main($scope){
-
+	$scope.username="";
 	var date = new Date();
 	$scope.timeVal = date.toTimeString();
 	$scope.updatefun = function(){
